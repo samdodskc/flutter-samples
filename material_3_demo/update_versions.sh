ec81f66ba9b23054c671df86fc7593a0691d9853
sdods <dods.sam@gmail.com>
2025-04-07 16:45:39 +0100

    created script to update to necessary target build dependency versions

---
+++ material_3_demo/update_versions.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+## Script to update the necessary files from templates
+
+file=android/app/build.gradle
+cat ${file}.template | sed "s/<JAVA_VERSION>/$TEMPLATE_JAVA_VERSION/" | sed "s/<JVM_TARGET_VERSION>/$TEMPLATE_JVM_TARGET_VERSION/" > $file
+
+file=android/gradle/wrapper/gradle-wrapper.properties
+cat ${file}.template | sed "s/<GRADLE_VERSION>/$TEMPLATE_GRADLE_VERSION/" > $file
+
+file=android/settings.gradle
+cat ${file}.template | sed "s/<GRADLE_PLUGIN_VERSION>/$TEMPLATE_GRADLE_PLUGIN_VERSION/" | sed "s/<KOTLIN_VERSION>/$TEMPLATE_KOTLIN_VERSION/" > $file
+
