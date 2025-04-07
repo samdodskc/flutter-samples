f92a653014e8f58abafce337045cf77d55e4a5e9
sdods <dods.sam@gmail.com>
2025-04-07 16:50:23 +0100

    moved update_versions script

--- material_3_demo/android/update_versions.sh
+++ material_3_demo/android/update_versions.sh
@@ -2,12 +2,12 @@
 
 ## Script to update the necessary files from templates
 
-file=android/app/build.gradle
+file=app/build.gradle
 cat ${file}.template | sed "s/<JAVA_VERSION>/$TEMPLATE_JAVA_VERSION/" | sed "s/<JVM_TARGET_VERSION>/$TEMPLATE_JVM_TARGET_VERSION/" > $file
 
-file=android/gradle/wrapper/gradle-wrapper.properties
+file=gradle/wrapper/gradle-wrapper.properties
 cat ${file}.template | sed "s/<GRADLE_VERSION>/$TEMPLATE_GRADLE_VERSION/" > $file
 
-file=android/settings.gradle
+file=settings.gradle
 cat ${file}.template | sed "s/<GRADLE_PLUGIN_VERSION>/$TEMPLATE_GRADLE_PLUGIN_VERSION/" | sed "s/<KOTLIN_VERSION>/$TEMPLATE_KOTLIN_VERSION/" > $file
 
